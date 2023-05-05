f4e4c527a14b9f80f851ff58af920feefa915756
Niraj Dasari <94426363+niraj-dasari@users.noreply.github.com>
2023-05-05 05:43:51 +0000

    Pending changes exported from your codespace

--- src/components/Employees.jsx
+++ src/components/Employees.jsx
@@ -1,9 +1,11 @@
-import data from '../assets/gems.json';
 import { useState, useEffect } from 'react';
 import Employee_details from './widgets/Employee-details';
+import axios from "axios";
+import { AiOutlineUserAdd } from "react-icons/ai";
 
 const Employees = () => {
-    const [empData,setEmpData] = useState(data);
+    const [data,setData] = useState([]);
+    const [empData, setEmpData] = useState(data);
     const [showModal, setShowModal] = useState(false);
     const [selectedEmployee, setSelectedEmployee] = useState('');
     const [searchString, setSearchString] = useState('');
@@ -18,6 +20,17 @@ const Employees = () => {
     };
 
     useEffect(() => {
+        const fetchData = async () => {
+            const result = await axios.get("https://niraj-dasari-musical-engine-jw6pv6w76q5fj5r7-5000.preview.app.github.dev/employees");
+            setData(result.data);
+            setEmpData(result.data);
+            console.log(result)
+        };
+
+        fetchData();
+    }, []);
+
+    useEffect(() => {
         if (showModal) {
             document.body.classList.add('overflow-hidden');
         } else {
@@ -26,18 +39,17 @@ const Employees = () => {
     }, [showModal]);
 
     const filterEmp = () => {
-        setEmpData(empData.filter(item => item['Employee Name'].includes(searchString)))
+        setEmpData(empData.filter(item => item['Employee Name'].toLowerCase().includes(searchString.toLowerCase())))
         console.log(empData);
     }
 
     const handleChange = (event) => {
         setSearchString(event.target.value);
-        if(event.target.value)
-        {
+        if (event.target.value) {
             setEmpData(data)
         }
         console.log(searchString);
-      }
+    }
 
     return (
         <div className="">
@@ -49,11 +61,22 @@ const Employees = () => {
                             <div class="relative mr-4 lg:w-full xl:w-1/2 w-2/4">
                                 <input type="text" onChange={handleChange} value={searchString} id="hero-field" name="hero-field" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:ring-2 focus:ring-orange-200 focus:bg-transparent focus:border-orange-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                             </div>
-                            <button class="inline-flex text-white bg-orange-500 border-0 py-2 px-6 focus:outline-none hover:bg-orange-600 rounded text-lg" onClick={() =>filterEmp()}>Search</button>
+                            <button class="inline-flex text-white bg-orange-500 border-0 py-2 px-6 focus:outline-none hover:bg-orange-600 rounded text-lg" onClick={() => filterEmp()}>Search</button>
                         </div>
                     </div>
-                    
+
                     <div className="flex flex-wrap -m-2">
+                                <div className="p-2 lg:w-1/3 md:w-1/2 w-full cursor-pointer">
+                                    <div className="h-full flex items-center border-orange-200 border p-4 rounded-lg bg-orange-200">
+                                        <div className="w-16 h-16 bg-orange-100 object-cover object-center flex justify-center items-center flex-shrink-0 rounded-full mr-4">
+                                            <AiOutlineUserAdd style={{ fontSize: '24px' }}/>
+                                        </div>
+                                        <div className="flex-grow">
+                                            <h2 className="text-gray-900 title-font font-medium">Employee name</h2>
+                                            <p className="text-gray-500">Employee email</p>
+                                        </div>
+                                    </div>
+                                </div>
                         {
                             empData.map((d) => (
                                 <div className="p-2 lg:w-1/3 md:w-1/2 w-full cursor-pointer" onClick={() => openModal(d['Emp Code'])} >
--- src/components/widgets/Employee-details.jsx
+++ src/components/widgets/Employee-details.jsx
@@ -1,6 +1,12 @@
-import { PaperClipIcon } from '@heroicons/react/20/solid';
+import axios from "axios";
 
 const Employee_details = (props) => {
+
+    const deleteEmp = (e) =>{
+        const result = axios.delete("https://niraj-dasari-musical-engine-jw6pv6w76q5fj5r7-5000.preview.app.github.dev/employees/"+e);
+        console.log(result)
+        props.onClose();
+    }
     console.log(props)
     return (
         <>
@@ -60,7 +66,7 @@ const Employee_details = (props) => {
                                         Preview
                                     </button>
                                 </div>
-                                <button type="button" className="inline-flex items-center text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-900">
+                                <button type="button" onClick={()=>deleteEmp(props.emp['Emp Code'])} className="inline-flex items-center text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-900">
                                     <svg aria-hidden="true" className="w-5 h-5 mr-1.5 -ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path></svg>
                                     Delete
                                 </button>
